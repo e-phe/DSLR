69bf034c337ad9489e67b9f6613e00fbe8f4de7c
ephe <ephe@student42.fr>
2021-11-19 11:59:32 +0100

    feat-pair_plot-* : bonus added

--- pair_plot.py
+++ pair_plot.py
@@ -1,23 +1,24 @@
 #!/usr/bin/env python3.8
 # -*- coding: utf-8 -*-
 
+import argparse
 import matplotlib.pyplot as plt
-import os
 import pandas as pd
 import seaborn as sns
 import sys
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Create a pair plot that help answer the following question : which characteristics will you use to train your coming logistic regressions?",
+    )
+    parser.add_argument("dataset_train.csv", help="dataset to use")
+    args = parser.parse_args()
     try:
-        if os.stat("datasets/dataset_train.csv").st_size > 0:
-            data = pd.read_csv("datasets/dataset_train.csv")
-        else:
-            sys.exit("Error")
+        df = pd.read_csv(sys.argv[1], index_col="Index")
     except:
         sys.exit("Error")
-    data.drop("Index", axis=1, inplace=True)
-    data = data[
-        ["Hogwarts House"] + list(data.select_dtypes(include="number"))
-    ].dropna()
-    sns.pairplot(data, hue="Hogwarts House")
+    df = df[["Hogwarts House"] + list(df.select_dtypes(include="number"))].dropna()
+    if df.empty:
+        sys.exit("Error")
+    sns.pairplot(df, hue="Hogwarts House")
     plt.show()
