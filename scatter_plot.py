69c61abd9e11ff5a40ec9255541a67084547f815
ephe <ephe@student42.fr>
2021-11-19 11:35:27 +0100

    feat-scatter_plot-* : change np by pd to open csv, added bonus usage

--- scatter_plot.py
+++ scatter_plot.py
@@ -1,63 +1,59 @@
 #!/usr/bin/env python3.8
 # -*- coding: utf-8 -*-
 
+import argparse
 import matplotlib.pyplot as plt
 import numpy as np
-import os
-import re
+import pandas as pd
 import sys
 
 
-def scatterPlot(data, features):
-    plt.xlabel(data[0, features[0]])
-    plt.ylabel(data[0, features[1]])
-    plt.scatter(
-        data[1:, features[0]].astype(float), data[1:, features[1]].astype(float)
-    )
+def scatterPlot(df, features):
+    plt.xlabel(df[0, features[0]])
+    plt.ylabel(df[0, features[1]])
+    plt.scatter(df[1:, features[0]].astype(float), df[1:, features[1]].astype(float))
     plt.show()
 
 
-def similarFeatures(data):
+def similarFeatures(df):
     longest = 0
     features = []
-    for i in range(0, len(data[0])):
-        for j in range(i + 1, len(data[0])):
-            similarities = len(set(data[1:, i]) & set(data[1:, j]))
+    for i in range(0, len(df[0])):
+        for j in range(i + 1, len(df[0])):
+            similarities = len(set(df[1:, i]) & set(df[1:, j]))
             if longest < similarities:
                 longest = similarities
                 features = [i, j]
     return features
 
 
-def prepData(data):
-    for i in range(0, len(data[0])):
-        for j in range(1, len(data)):
-            data[j, i] = str(abs(float(data[j, i]))).replace(".", "")
-    return data
+def prepData(df):
+    for i in range(0, len(df[0])):
+        for j in range(1, len(df)):
+            df[j, i] = str(abs(float(df[j, i]))).replace(".", "")
+    return df
 
 
-def delCoLette(data, i):
-    for j in range(1, len(data)):
-        if (
-            data[j, i] != str(np.nan)
-            and re.search("^[+-]?([0-9]+[.])?[0-9]+$", data[j, i]) == None
-        ):
-            return [np.delete(data, i, 1), i]
-    return [data, i + 1]
+def parse():
+    parser = argparse.ArgumentParser(
+        description="Create a scatter plot that help answer the following question : which two features are similar?",
+    )
+    parser.add_argument("dataset_train.csv", help="dataset to use")
+    args = parser.parse_args()
+    try:
+        df = pd.read_csv(sys.argv[1], index_col="Index")
+    except:
+        sys.exit("Error")
+    df = df.select_dtypes(exclude=[object])
+    if df.empty:
+        sys.exit("Error")
+    return df
 
 
 if __name__ == "__main__":
-    try:
-        if os.stat("datasets/dataset_train.csv").st_size > 0:
-            data = np.loadtxt("datasets/dataset_train.csv", dtype=str, delimiter=",")
-        else:
-            sys.exit("Error")
-    except:
-        sys.exit("Error")
-    data = np.delete(data, 0, 1)
-    data[data == ""] = np.nan
-    i = 0
-    while i < len(data[0]):
-        [data, i] = delCoLette(data, i)
-    features = similarFeatures(prepData(data.copy()))
-    scatterPlot(data, features)
+    df = parse()
+    col = np.array(df.columns)
+    df = np.array(df, dtype="<U1000")
+    df = np.insert(df, 0, col, axis=0)
+    features = similarFeatures(prepData(df.copy()))
+    scatterPlot(df, features)
